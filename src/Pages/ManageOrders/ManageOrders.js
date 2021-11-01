55d6ff982497266bc22ba52ce7443ab2e6f79f39
shahidmonowarr <shahidmonowar21@gmail.com>
2021-11-01 10:16:54 +0600

    d

--- src/Pages/ManageOrders/ManageOrders.js
+++ src/Pages/ManageOrders/ManageOrders.js
@@ -7,29 +7,32 @@ const ManageOrders = () => {
     useEffect(() => {
         fetch('https://whispering-mountain-08935.herokuapp.com/orders')
             .then(res => res.json())
-            .then(data => console.log(data));
+            .then(data => setOrders(data));
     }, []);
 
     const handleDelete = id => {
-        const url = `https://whispering-mountain-08935.herokuapp.com/orders/${id}`;
-        fetch(url, {
-            method: 'DELETE'
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                if (data.deletedCount) {
-                    alert('Package deleted')
-                    const remaining = orders.filter(singlePackage => singlePackage._id !== id);
-                    setOrders(remaining);
-                }
-
+        const proceed = window.confirm("Are You sure, You want to delete?");
+        if (proceed) {
+            const url = `https://whispering-mountain-08935.herokuapp.com/orders/${id}`;
+            fetch(url, {
+                method: 'DELETE'
             })
-    }
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount) {
+                        alert('Order deleted Successfully')
+                        const remaining = orders.filter(singlePackage => singlePackage._id !== id);
+                        setOrders(remaining);
+                    }
 
+                })
+        }
+
+    }
     return (
         <div className="manage-orders">
-            <h2 className="text-success fw-bold pt-5 pb-3 fs-1">Remove package If Needed</h2>
+            <h2 className="text-success fw-bold pt-5 pb-3 fs-1">Remove and Update</h2>
             <br />
             {
                 orders.map(order => <div key={order._id}>
